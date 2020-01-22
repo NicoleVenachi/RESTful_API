70faeb039292d544f5c384bf1444dfba560295d6
NicoleVenachi <ludwing@unicauca.edu.co>
2020-01-22 05:47:10 -0500

    Cap3_PeticionREST_cambio en la respuesta PHP

--- Curso_en_JS/Cap3/xkcd.js
+++ Curso_en_JS/Cap3/xkcd.js
@@ -3,5 +3,5 @@ const axios = require('axios')
 const url = 'https://xkcd.com/info.0.json'
 
 axios.get(url)
-  .then(res => console.log(res.data))
+  .then(res => console.log(res.data.img))
   .catch(err => console.log(err))
--- Curso_en_PHP/Cap3/xkcd.php
+++ Curso_en_PHP/Cap3/xkcd.php
@@ -1,2 +1,6 @@
 <?php
-echo file_get_contents('https://xkcd.com/info.0.json').PHP_EOL; //Abre y muestra contenido de un arhivo, luego imprime espacio
+// echo file_get_contents('https://xkcd.com/info.0.json').PHP_EOL; //Abre y muestra contenido de un arhivo, luego imprime espacio
+$json = file_get_contents('https://xkcd.com/info.0.json'); //guardo info en string JSON
+$data = json_decode($json,true); //areglo basado en string, true significa que lo mantiene como arreglo, no como obj
+echo $data['img'].PHP_EOL; //muestro prop img
+
